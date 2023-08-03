c77a2e5c2f62fbc0f5e7215b920b22d138ab3251
st22143 <103467677+st22143@users.noreply.github.com>
2023-08-03 18:52:04 +1200

    javascript folder and file ready

---
+++ Javascript/scripts.js
@@ -0,0 +1,23 @@
+function cabin_popup() {
+    document.getElementById("cabinframe").style.backgroundColor = "rgba(25, 25, 25, 0.518)";
+}
+
+function information_popup() {
+    document.getElementById("informationframe").style.backgroundColor = "rgba(25, 25, 25, 0.518)";
+}
+
+function skiing_popup() {
+    document.getElementById("skiingframe").style.backgroundColor = "rgba(25, 25, 25, 0.518)";
+}
+
+function undo_cabin() {
+    document.getElementById("cabinframe").style.backgroundColor = "";
+}
+
+function undo_information() {
+    document.getElementById("informationframe").style.backgroundColor = "";
+}
+
+function undo_skiing() {
+    document.getElementById("skiingframe").style.backgroundColor = "";
+}
