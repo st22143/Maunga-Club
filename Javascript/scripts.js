617a459255f3c6844e312e30ef0d75aecb2055d3
st22143 <103467677+st22143@users.noreply.github.com>
2023-08-20 20:39:01 +1200

    finished all the functionality for the cabins page

--- Javascript/scripts.js
+++ Javascript/scripts.js
@@ -42,34 +42,180 @@ function openkereru() {
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() { //Checks calls the following code if the website is loaded
 
-    var Image1 = document.getElementById("kakapoimage1");
-    var Image2 = document.getElementById("kakapoimage2");
-    var Image3 = document.getElementById("kakapoimage3");
+    var kakapoimage1 = document.getElementById("kakapoimage1"); //Creating variables to make my life easier
+    var kakapoimage2 = document.getElementById("kakapoimage2");
+    var kakapoimage3 = document.getElementById("kakapoimage3");
 
-    let images = [Image1, Image2, Image3];
+    let kakapoimages = [kakapoimage1, kakapoimage2, kakapoimage3];
 
-    if (Image1 && Image2 && Image3) {
-        Image1.style.display = "block";
-        Image2.style.display = "none";
-        Image3.style.display = "none";
-        number = 0;
+    if (kakapoimages[0], kakapoimages[1], kakapoimages[2]) { //The following code will only be called if these variables exist
+        kakapoimages[0].style.display = "block"; //One image will be displayed
+        kakapoimages[1].style.display = "none";
+        kakapoimages[2].style.display = "none";
+        number = 0; 
 
-        setInterval(function(){
+        setInterval(function(){  //The following code will be called every 300 milliseconds (3 seconds)
             
-            for(i=0; i<=2; i++) {
-                images[i].style.display = "none";
+            for(i=0; i<=2; i++) { //For loop for javascript
+                kakapoimages[i].style.display = "none"; //Makes all images display nothing.
+            }
+            
+            
+            console.log(number);
+            
+            if(number<=2) {
+                kakapoimages[number].style.display = "block"; //One image will be displayed
             }
             
             number++;
+            
+            if(number>=3) {
+                number=0;
+            }
+            
 
-            images[number].style.display = "block"; 
+        }, 3000); 
+    }
+    
+    var pukekoimage1 = document.getElementById("pukekoimage1"); //Creating variables to make my life easier
+    var pukekoimage2 = document.getElementById("pukekoimage2");
+    var pukekoimage3 = document.getElementById("pukekoimage3");
 
-            if(number>=2) {
-                number=-1;
-            }}, 
-            3000);
-        }
+    let pukekoimages = [pukekoimage1, pukekoimage2, pukekoimage3];
 
-});
+    if (pukekoimages[0], pukekoimages[1], pukekoimages[2]) { //The following code will only be called if these variables exist
+        pukekoimages[0].style.display = "block"; //One image will be displayed
+        pukekoimages[1].style.display = "none";
+        pukekoimages[2].style.display = "none";
+        number = 0; 
+
+        setInterval(function(){  //The following code will be called every 300 milliseconds (3 seconds)
+            
+            for(i=0; i<=2; i++) { //For loop for javascript
+                pukekoimages[i].style.display = "none"; //Makes all images display nothing.
+            }
+            
+            
+            console.log(number);
+            
+            if(number<=2) {
+                pukekoimages[number].style.display = "block"; //One image will be displayed
+            }
+            
+            number++;
+            
+            if(number>=3) {
+                number=0;
+            }
+            
+
+        }, 3000); 
+    }
+
+    var keruruimage1 = document.getElementById("keruruimage1"); //Creating variables to make my life easier
+    var keruruimage2 = document.getElementById("keruruimage2");
+    var keruruimage3 = document.getElementById("keruruimage3");
+
+    let keruruimages = [keruruimage1, keruruimage2, keruruimage3];
+
+    if (keruruimages[0], keruruimages[1], keruruimages[2]) { //The following code will only be called if these variables exist
+        keruruimages[0].style.display = "block"; //One image will be displayed
+        keruruimages[1].style.display = "none";
+        keruruimages[2].style.display = "none";
+        number = 0; 
+
+        setInterval(function(){  //The following code will be called every 300 milliseconds (3 seconds)
+            
+            for(i=0; i<=2; i++) { //For loop for javascript
+                keruruimages[i].style.display = "none"; //Makes all images display nothing.
+            }
+            
+            
+            console.log(number);
+            
+            if(number<=2) {
+                keruruimages[number].style.display = "block"; //One image will be displayed
+            }
+            
+            number++;
+            
+            if(number>=3) {
+                number=0;
+            }
+            
+
+        }, 3000); 
+    } 
+}); */
+
+
+document.addEventListener("DOMContentLoaded", function() { //Checks calls the following code if the website is loaded
+
+    function switchImages(images) {
+        let number = 0;
+
+        images[number].style.display = "block";
+        images.forEach((image, index) => {
+            if (index !== number) {
+                image.style.display = "none";
+            }
+        });
+
+        setInterval(function() {
+            images[number].style.display = "none";
+
+           
+
+            number++;
+
+            if (number >= images.length) {
+                number = 0;
+            }
+
+            images[number].style.display = "block";
+        }, 3000);
+    }
+
+    var kakapoimages = [
+        document.getElementById("kakapoimage1"),
+        document.getElementById("kakapoimage2"),
+        document.getElementById("kakapoimage3")
+    ];
+
+    var pukekoimages = [
+        document.getElementById("pukekoimage1"),
+        document.getElementById("pukekoimage2"),
+        document.getElementById("pukekoimage3")
+    ];
+
+    var keruruimages = [
+        document.getElementById("keruruimage1"),
+        document.getElementById("keruruimage2"),
+        document.getElementById("keruruimage3")
+    ];
+
+    if (kakapoimages.every(image => image != null)) {  //The functions will only be called if the lists are valid
+        switchImages(kakapoimages);
+    }
+    
+    if (pukekoimages.every(image => image != null)) {
+        switchImages(pukekoimages);
+    }
+    
+    
+    if (keruruimages.every(image => image != null)) {
+        switchImages(keruruimages);
+    }
+
+}); 
+
+function pop_up(){
+    console.log("working");
+    document.getElementById("details").style.display = "block";
+}
+
+function testing(){
+    document.getElementById("details").style.display = "none";
+}
